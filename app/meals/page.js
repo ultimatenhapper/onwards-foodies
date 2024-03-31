065e1294f5118b036726704f0e1053b527bf49b7
Javi <belikemike3057@hotmail.com>
2024-03-31 10:50:24 +0200

    Not found, error and loading with Suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,22 +1,40 @@
+import { Suspense } from "react";
+
 import Link from "next/link";
-import styles from "./page.module.css";
+
 import MealsGrid from "../components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+import styles from "./page.module.css";
 
 function Meals() {
+  const meals = getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+function MealsPage() {
   return (
     <>
       <header className={styles.header}>
-        <h1>Delicious meals, created <span className={styles.highlight}>by you</span></h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun</p>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
         <p className={styles.cta}>
           <Link href="/meals/share">Share your favorite recipe</Link>
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<p className={styles.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
 }
 
-export default Meals;
+export default MealsPage;
